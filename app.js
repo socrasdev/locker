e740d044e56807854c48159ba4b4c2af437e9199
socrasdev <53356050+socrasdev@users.noreply.github.com>
2019-07-26 19:42:39 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+$(".protected-content").sociallocker({
+    theme: "glass",
+    locker: {
+        timer: 360
+    },
+    buttons: {
+        order: ["facebook-like"]
+    },
+    facebook: {
+        appId: "493640178088282",
+        like: {
+            url: "https://www.facebook.com/hackFlixEs"
+        }
+    }
+});
