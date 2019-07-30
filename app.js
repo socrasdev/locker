55541af425cc62e1d7f7135b53d219313d900832
socrasdev <53356050+socrasdev@users.noreply.github.com>
2019-07-29 20:03:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 $(".protected-content").sociallocker({
     theme: "glass",
+    text: {
+header: "Enlaces Protegidos",
+message: "Presiona el boton like para desbloquear los enlaces"
+},
+    locker: {
+timer: 60
+},
     buttons: {
         order: ["facebook-like"]
     },
